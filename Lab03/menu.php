9081792c4b3fe6fe97c487b6cf4ade69904602d1
unknown <6611130085@mut.ac.th>
2025-07-17 17:07:03 +0700

    commit5

--- Lab03/menu.php
+++ Lab03/menu.php
@@ -1,12 +1,9 @@
 <?php
      echo "Home";
      echo "Product"
-<<<<<<< HEAD
      echo "Mahanakorn"
 ?>
-=======
 
      echo "about";
      echo "Map"
 ?>
->>>>>>> 89da7f2f55912f8a3ed6017574b930271e7127f3
